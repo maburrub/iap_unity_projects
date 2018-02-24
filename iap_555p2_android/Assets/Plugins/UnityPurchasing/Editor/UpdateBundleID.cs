ec894cc27a4f53b12f671b4e21d28af4f6afed0e
Mohammed Abualrob <mohammed@unity3d.com>
2018-02-24 15:50:38 -0800

    Adding update bundle id

---
+++ iap_555p2_android/Assets/Plugins/UnityPurchasing/Editor/UpdateBundleID.cs
@@ -0,0 +1,16 @@
+﻿// C# example:
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class UpdateBundleID {
+	[PostProcessBuildAttribute(1)]
+	public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) 
+	{
+		string filename = pathToBuiltProject + "/Contents/Info.plist";
+		var fileContents = System.IO.File.ReadAllText(filename);
+		fileContents = fileContents.Replace("unity.Unity3d.IAPDemo", "com.unity3d.unityiap.mac.demo"); 
+		fileContents = fileContents.Replace("com.Company.ProductName", "com.unity3d.unityiap.mac.demo"); 
+		System.IO.File.WriteAllText(filename, fileContents);
+	}
+}
